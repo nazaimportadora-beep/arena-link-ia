abe056962c7d6f08ecb6b186f4df1ce1c98c921e
nazaimportadora-beep <nazaimportadora@gmail.com>
2025-12-10 22:42:33 -0300

    Create input.tsx

---
+++ components/ui/components/ui/components/ui/input.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className = "", ...props }, ref) => {
+    return (
+      <input
+        ref={ref}
+        className={`w-full border border-gray-300 rounded-md px-3 py-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 ${className}`}
+        {...props}
+      />
+    );
+  }
+);
+
+Input.displayName = "Input";
+
+export default Input;
